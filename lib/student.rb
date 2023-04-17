318f55ae4fddf16df18ed9e69fe2340ed142296c
bwrosen20 <bwrosen20@gmail.com>
2023-04-16 20:13:59 -0400

    commit tree b35f0fd018715dc6369f358085ac904c39f1fedb

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+require "pry"
+require_relative './user'
+
+class Student < User
+
+    attr_accessor :first_name, :last_name
+    attr_reader :knowledge
+
+    def initialize(knowledge=[])
+        @knowledge=knowledge
+    end
+
+    def learn(fact)
+
+        self.knowledge << fact
+
+    end
 
 end
+
+binding.pry
